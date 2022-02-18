80f0062b4bcac9cb1be9170daa258edf1f704921
nhatminhnb113 <85503042+nhatminhnb113@users.noreply.github.com>
2022-02-18 15:03:35 +0700

    Delete CourseManagement directory

--- CourseManagement/src/main/java/com/main/coursemanagement/repository/StudentsRepo.java
+++
@@ -1,6 +0,0 @@
-package com.main.coursemanagement.repository;
-import com.main.coursemanagement.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentsRepo extends JpaRepository<Student, Integer> {
-}
